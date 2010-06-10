5aad4f015c1efaa3b05835bd7ea4eb0357b5eb58
Giacomo Tesio <giacomo@tesio.it>
2010-06-10 17:16:05 +0200

    NotSpecification tests

---
+++ Challenges/Challenge00.DDDSample/UnitTests/Challenge00.DDDSample.Default.UnitTests/Shared/NotSpecificationTester.cs
@@ -0,0 +1,105 @@
+using NUnit.Framework;
+using System;
+using Challenge00.DDDSample.Default.Shared;
+using Challenge00.DDDSample.Shared;
+using Rhino.Mocks;
+namespace Challenge00.DDDSample.Default.UnitTests
+{
+	[TestFixture()]
+	public class NotSpecificationTester : AbstractSpecificationTester<NotSpecification<object>, object>
+	{
+		#region implemented abstract members of Challenge00.DDDSample.Default.UnitTests.AbstractSpecificationTester[NotSpecification[System.Object],System.Object]
+		protected override void CreateEqualsSpecification (out NotSpecification<object> spec1, out NotSpecification<object> spec2)
+		{
+			ISpecification<object> n1 = MockRepository.GenerateMock<ISpecification<object>>();
+			ISpecification<object> n2 = MockRepository.GenerateMock<ISpecification<object>>();
+			n1.Expect(s => s.Equals(n2)).Return(true);
+			n2.Expect(s => s.Equals(n1)).Return(true);
+			
+			spec1 = new NotSpecification<object>(n1);
+			spec2 = new NotSpecification<object>(n2);
+		}
+		
+		
+		protected override void CreateDifferentSpecification (out NotSpecification<object> spec1, out NotSpecification<object> spec2)
+		{
+			ISpecification<object> n1 = MockRepository.GenerateMock<ISpecification<object>>();
+			ISpecification<object> n2 = MockRepository.GenerateMock<ISpecification<object>>();
+			n1.Expect(s => s.Equals(n2)).Return(false);
+			n2.Expect(s => s.Equals(n1)).Return(false);
+			
+			spec1 = new NotSpecification<object>(n1);
+			spec2 = new NotSpecification<object>(n2);
+		}
+		
+		
+		protected override NotSpecification<object> CreateNewSpecification ()
+		{
+			ISpecification<object> n1 = MockRepository.GenerateMock<ISpecification<object>>();
+			n1.Expect(s => s.Equals(s)).Return(true);
+			
+			return new NotSpecification<object>(n1);
+		}
+		
+		#endregion
+		
+		[Test]
+		public void Test_Ctor_01()
+		{
+			// arrange:
+			ISpecification<object> toNegate = MockRepository.GenerateMock<ISpecification<object>>();
+		
+			// act:
+			NotSpecification<object> spec = new NotSpecification<object>(toNegate);
+		
+			// assert:
+			Assert.IsNotNull(spec);
+			Assert.AreSame(toNegate, spec.Negated);
+		}
+		
+		[Test()]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void Test_Ctor_02 ()
+		{
+			// arrange:
+			
+		
+			// act:
+			new NotSpecification<object>(null);
+		}
+		
+		[Test]
+		public void Test_IsSatisfiedBy_01()
+		{
+			// arrange:
+			object candidate = new object();
+			ISpecification<object> toNegate = MockRepository.GenerateMock<ISpecification<object>>();
+			toNegate.Expect(s => s.IsSatisfiedBy(candidate)).Return(true);
+		
+			// act:
+			NotSpecification<object> spec = new NotSpecification<object>(toNegate);
+			bool satisfied = spec.IsSatisfiedBy(candidate);
+		
+			// assert:
+			Assert.IsFalse(satisfied);
+			toNegate.VerifyAllExpectations();
+		}
+		
+		[Test]
+		public void Test_IsSatisfiedBy_02()
+		{
+			// arrange:
+			object candidate = new object();
+			ISpecification<object> toNegate = MockRepository.GenerateMock<ISpecification<object>>();
+			toNegate.Expect(s => s.IsSatisfiedBy(candidate)).Return(false);
+		
+			// act:
+			NotSpecification<object> spec = new NotSpecification<object>(toNegate);
+			bool satisfied = spec.IsSatisfiedBy(candidate);
+		
+			// assert:
+			Assert.IsTrue(satisfied);
+			toNegate.VerifyAllExpectations();
+		}
+	}
+}
---
+++ Challenges/Challenge00.DDDSample/UnitTests/Challenge00.DDDSample.Default.UnitTests/Shared/OrSpecificationTester.cs
@@ -0,0 +1,176 @@
+using System;
+using NUnit.Framework;
+using Challenge00.DDDSample.Default.Shared;
+using Challenge00.DDDSample.Shared;
+using Rhino.Mocks;
+namespace Challenge00.DDDSample.Default.UnitTests
+{
+	[TestFixture]
+	public class OrSpecificationTester : AbstractSpecificationTester<OrSpecification<object>, object>
+	{
+		#region implemented abstract members of Challenge00.DDDSample.Default.UnitTests.AbstractSpecificationTester[OrSpecification[System.Object],System.Object]
+		protected override void CreateEqualsSpecification (out OrSpecification<object> spec1, out OrSpecification<object> spec2)
+		{
+			ISpecification<object> left = MockRepository.GenerateMock<ISpecification<object>>();
+			left.Stub(s => s.Equals(left)).Return(true).Repeat.Any();
+			ISpecification<object> right = MockRepository.GenerateMock<ISpecification<object>>();
+			right.Stub(s => s.Equals(right)).Return(true).Repeat.Any();
+			
+			right.Stub(s => s.Equals(left)).Return(false).Repeat.Any();
+			left.Stub(s => s.Equals(right)).Return(false).Repeat.Any();
+			
+			spec1 = new OrSpecification<object>(left, right);
+			spec2 = new OrSpecification<object>(left, right);
+		}
+		
+		
+		protected override void CreateDifferentSpecification (out OrSpecification<object> spec1, out OrSpecification<object> spec2)
+		{
+			ISpecification<object> left = MockRepository.GenerateMock<ISpecification<object>>();
+			left.Stub(s => s.Equals(left)).Return(true).Repeat.Any();
+			ISpecification<object> right = MockRepository.GenerateMock<ISpecification<object>>();
+			right.Stub(s => s.Equals(right)).Return(true).Repeat.Any();
+			
+
+			spec1 = new OrSpecification<object>(right, right);
+			spec2 = new OrSpecification<object>(left, left);
+		}
+		
+		
+		protected override OrSpecification<object> CreateNewSpecification ()
+		{
+			ISpecification<object> left = MockRepository.GenerateMock<ISpecification<object>>();
+			left.Stub(s => s.Equals(left)).Return(true).Repeat.Any();
+			ISpecification<object> right = MockRepository.GenerateMock<ISpecification<object>>();
+			right.Stub(s => s.Equals(right)).Return(true).Repeat.Any();
+			
+			right.Stub(s => s.Equals(left)).Return(false).Repeat.Any();
+			left.Stub(s => s.Equals(right)).Return(false).Repeat.Any();
+			
+			return new OrSpecification<object>(left, right);
+		}
+		
+		#endregion
+		
+		[Test]
+		public void Test_Ctor_01()
+		{
+			// arrange:
+			ISpecification<object> left = MockRepository.GenerateMock<ISpecification<object>>();
+			ISpecification<object> right = MockRepository.GenerateMock<ISpecification<object>>();	
+			
+			// act:
+			OrSpecification<object> spec = new OrSpecification<object>(left,right);
+		
+			// assert:
+			Assert.IsNotNull(spec);
+			Assert.AreSame(left, spec.First);
+			Assert.AreSame(right, spec.Second);
+		}
+		
+		[Test()]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void Test_Ctor_02 ()
+		{
+			// arrange:
+			ISpecification<object> right = MockRepository.GenerateMock<ISpecification<object>>();	
+			
+			// act:
+			OrSpecification<object> spec = new OrSpecification<object>(null,right);
+		}
+		
+		[Test()]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void Test_Ctor_03 ()
+		{
+			// arrange:
+			ISpecification<object> left = MockRepository.GenerateMock<ISpecification<object>>();	
+			
+			// act:
+			OrSpecification<object> spec = new OrSpecification<object>(left,null);
+		}
+		
+		[Test]
+		public void Test_IsSatisfiedBy_01()
+		{
+			// arrange:
+			object candidate = new object();
+			ISpecification<object> left = MockRepository.GenerateMock<ISpecification<object>>();
+			left.Expect(s => s.IsSatisfiedBy(candidate)).Return(true);
+			ISpecification<object> right = MockRepository.GenerateMock<ISpecification<object>>();	
+			
+		
+			// act:
+			ISpecification<object> target = new OrSpecification<object>(left, right);
+			bool satisfied = target.IsSatisfiedBy(candidate);
+		
+			// assert:
+			Assert.IsTrue(satisfied);
+		 	left.VerifyAllExpectations();
+			right.AssertWasNotCalled(s => s.IsSatisfiedBy(candidate));
+		}
+		
+		[Test]
+		public void Test_IsSatisfiedBy_02()
+		{
+			// arrange:
+			object candidate = new object();
+			ISpecification<object> left = MockRepository.GenerateMock<ISpecification<object>>();
+			left.Expect(s => s.IsSatisfiedBy(candidate)).Return(true);
+			ISpecification<object> right = MockRepository.GenerateMock<ISpecification<object>>();	
+			
+		
+			// act:
+			ISpecification<object> target = new OrSpecification<object>(left, right);
+			bool satisfied = target.IsSatisfiedBy(candidate);
+		
+			// assert:
+			Assert.IsTrue(satisfied);
+		 	left.VerifyAllExpectations();
+			right.AssertWasNotCalled(s => s.IsSatisfiedBy(candidate));
+		}
+		
+		[Test]
+		public void Test_IsSatisfiedBy_03()
+		{
+			// arrange:
+			object candidate = new object();
+			ISpecification<object> left = MockRepository.GenerateMock<ISpecification<object>>();
+			left.Expect(s => s.IsSatisfiedBy(candidate)).Return(false);
+			ISpecification<object> right = MockRepository.GenerateMock<ISpecification<object>>();	
+			right.Expect(s => s.IsSatisfiedBy(candidate)).Return(true);
+
+		
+			// act:
+			ISpecification<object> target = new OrSpecification<object>(left, right);
+			bool satisfied = target.IsSatisfiedBy(candidate);
+		
+			// assert:
+			Assert.IsTrue(satisfied);
+		 	left.VerifyAllExpectations();
+			right.VerifyAllExpectations();
+		}
+		
+		[Test]
+		public void Test_IsSatisfiedBy_04()
+		{
+			// arrange:
+			object candidate = new object();
+			ISpecification<object> left = MockRepository.GenerateMock<ISpecification<object>>();
+			left.Expect(s => s.IsSatisfiedBy(candidate)).Return(false);
+			ISpecification<object> right = MockRepository.GenerateMock<ISpecification<object>>();	
+			right.Expect(s => s.IsSatisfiedBy(candidate)).Return(false);
+			
+		
+			// act:
+			ISpecification<object> target = new OrSpecification<object>(left, right);
+			bool satisfied = target.IsSatisfiedBy(candidate);
+		
+			// assert:
+			Assert.IsFalse(satisfied);
+		 	left.VerifyAllExpectations();
+            right.VerifyAllExpectations();
+        }
+	}
+}
+
