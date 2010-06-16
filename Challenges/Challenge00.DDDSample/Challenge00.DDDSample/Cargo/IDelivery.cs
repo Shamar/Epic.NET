464a16ccf0751abd667817a7551381417526093e
Giacomo Tesio <giacomo@tesio.it>
2010-06-16 19:17:02 +0200

    more delivery

--- Challenges/Challenge00.DDDSample/Challenge00.DDDSample/Cargo/IDelivery.cs
+++ Challenges/Challenge00.DDDSample/Challenge00.DDDSample/Cargo/IDelivery.cs
@@ -1,5 +1,6 @@
 using System;
 using Challenge00.DDDSample.Voyage;
+using Challenge00.DDDSample.Location;
 namespace Challenge00.DDDSample
 {
 	/// <summary>
@@ -21,7 +22,30 @@ namespace Challenge00.DDDSample
 		/// <summary>
 		/// Estimated time of arrival.
 		/// </summary>
-		DateTime estimatedTimeOfArrival { get; }
+		DateTime EstimatedTimeOfArrival { get; }
+		
+		/// <summary>
+		/// True if the cargo has been unloaded at the final destination. 
+		/// </summary>
+		bool IsUnloadedAtDestination { get; }
+		
+		/// <summary>
+		/// True if cargo is misdirected.  
+		/// </summary>
+		/// <remarks>
+		/// <list type="bullet">
+		/// <item>A cargo is misdirected if it is in a location that's not in the itinerary.</item>
+		/// <item>A cargo with no itinerary can not be misdirected.</item>
+		/// <item>A cargo that has received no handling events can not be misdirected.</item>>
+		/// </list>
+		/// </remarks>
+		bool IsMisdirected { get; }
+		
+		/// <summary>
+		/// Last known location of the cargo, or <see cref="Location.Unknown"/> if 
+		/// the delivery history is empty 
+		/// </summary>
+		ILocation LastKnownLocation { get; }
 		
 	}
 }
