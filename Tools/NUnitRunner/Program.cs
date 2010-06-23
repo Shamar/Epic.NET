81ec9c20181cf1dff27f65f0115bc6b20f840197
Giacomo Tesio <giacomo@tesio.it>
2010-06-23 11:52:33 +0200

    NUnit 2.5.5 with a dedicated NUnitRunner project

---
+++ Tools/NUnitRunner/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+using NUnit.Gui;
+
+namespace NUnitRunner
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            AppEntry.Main(new string[0]);
+        }
+    }
+}
