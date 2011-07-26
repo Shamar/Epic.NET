23d9be244199e628383afb7b2ca5f77eee9153a2
Giacomo Tesio <giacomo@tesio.it>
2011-07-26 12:36:49 +0200

    TemplateVisitor as stand alone (no more part of a composition)

--- Code/Epic.Linq/Expressions/Templates/TemplateVisitor.cs
+++ Code/Epic.Linq/Expressions/Templates/TemplateVisitor.cs
@@ -24,32 +24,15 @@
 using System;
 using Epic.Linq.Expressions.Visit;
 using System.Linq.Expressions;
+using System.Collections.Generic;
 
 namespace Epic.Linq.Expressions.Templates
 {
-    public sealed class TemplateVisitor<TTemplateExpression> : VisitorsComposition.VisitorBase, ICompositeVisitor<MethodCallExpression>
+    public sealed class TemplateVisitor<TTemplateExpression> : ICompositeVisitor<TTemplateExpression>
         where TTemplateExpression : Expression
     {
-        private readonly VisitorWrapper<TTemplateExpression> _initializer;
-        
-        public TemplateVisitor (VisitorsComposition composition)
-            : base(composition)
+        public TemplateVisitor ()
         {
-            _initializer = new VisitorWrapper<TTemplateExpression>(this, InitializeVisit);
-        }
-        
-        internal protected override ICompositeVisitor<TExpression> AsVisitor<TExpression> (TExpression target)
-        {
-            ICompositeVisitor<TExpression> visitor = base.AsVisitor<TExpression>(target);
-            if(null == visitor || !IsQueryAccess(target as MethodCallExpression))
-            {
-                visitor = _initializer as ICompositeVisitor<TExpression>;
-                if(null == visitor)
-                {
-                    visitor = new VisitorWrapper<TExpression>(this, this.Parse<TExpression>);
-                }
-            }
-            return visitor;
         }
         
         private static bool IsQueryAccess(MethodCallExpression call)
@@ -65,42 +48,190 @@ namespace Epic.Linq.Expressions.Templates
         {
             state = state.Add<QueryDataExtractor<TTemplateExpression>>(new QueryDataExtractor<TTemplateExpression>());
             state = state.Add<ExpressionPath<TTemplateExpression>>(new ExpressionPath<TTemplateExpression>(e => e));
-            ICompositeVisitor<TTemplateExpression> nextVisitor = GetNextVisitor<TTemplateExpression>(expression);
-            return nextVisitor.Visit(expression, state);
+            return ParseExpression(expression, state);
+        }
+            
+        #region tracking path methods
+        
+        private Expression ParseUnaryExpression(UnaryExpression expression, IVisitState state)
+        {
+            return expression;
         }
         
-        private Expression Parse<TExpression>(TExpression target, IVisitState state) where TExpression : Expression
+        private Expression ParseBinaryExpression(BinaryExpression expression, IVisitState state)
         {
-            ExpressionPath<TExpression> path = null;
-            state.TryGet<ExpressionPath<TExpression>>(out path);
-            
-            // TODO : visit logic here
-            
-            ICompositeVisitor<TExpression> nextVisitor = GetNextVisitor<TExpression>(target);
-            return nextVisitor.Visit(target, state);
+            throw new NotImplementedException();
         }
 
-        #region ICompositeVisitor[MethodCallExpression] implementation
-        public Expression Visit (MethodCallExpression target, IVisitState state)
+        private Expression ParseConditionalExpression (ConditionalExpression expression, IVisitState state)
         {
-            // read argument[0]
-            ConstantExpression nameExpression = target.Arguments[0] as ConstantExpression; // TODO : handle closures
-            string name = nameExpression.Value as string;
+            throw new NotImplementedException ();
+        }
+
+        private Expression ParseConstantExpression (ConstantExpression expression, IVisitState state)
+        {
+            throw new NotImplementedException ();
+        }
+
+        private Expression ParseInvocationExpression (InvocationExpression expression, IVisitState state)
+        {
+            throw new NotImplementedException ();
+        }
+
+        private Expression ParseLambdaExpression (LambdaExpression expression, IVisitState state)
+        {
+            throw new NotImplementedException ();
+        }
+
+        private Expression ParseMemberExpression (MemberExpression expression, IVisitState state)
+        {
+            throw new NotImplementedException ();
+        }
+
+        private Expression ParseMethodCallExpression (MethodCallExpression expression, IVisitState state)
+        {
+            if(IsQueryAccess(expression))
+            {
+                // read argument[0]
+                ConstantExpression nameExpression = expression.Arguments[0] as ConstantExpression; // TODO : handle closures
+                string name = nameExpression.Value as string;
+                
+                // register the path to here in the extractor
+                QueryDataExtractor<TTemplateExpression> extractor = null;
+                state.TryGet<QueryDataExtractor<TTemplateExpression>>(out extractor);
+                ExpressionPath<MethodCallExpression> path = null;
+                state.TryGet<ExpressionPath<MethodCallExpression>>(out path);
+                
+                path.Register(name, extractor);
+            }
+            else
+            {
+                // Parse the MethodCallExpression
+            }
+            return expression;
+        }
+
+        private Expression ParseNewExpression (NewExpression expression, IVisitState state)
+        {
+            throw new NotImplementedException ();
+        }
+
+        private Expression ParseMemberInitExpression (MemberInitExpression expression, IVisitState state)
+        {
+            throw new NotImplementedException ();
+        }
+
+        private Expression ParseNewArrayExpression (NewArrayExpression expression, IVisitState state)
+        {
+            throw new NotImplementedException ();
+        }
+
+        private Expression ParseListInitExpression (ListInitExpression expression, IVisitState state)
+        {
+            throw new NotImplementedException ();
+        }
+
+        private Expression ParseParameterExpression (ParameterExpression expression, IVisitState state)
+        {
+            throw new NotImplementedException ();
+        }
+
+        private Expression ParseTypeBinaryExpression (TypeBinaryExpression expression, IVisitState state)
+        {
+            throw new NotImplementedException ();
+        }
+        
+        private Expression ParseExpression(Expression expression, IVisitState state)
+        {
+            ExpressionPath<Expression> path = null;
+            state.TryGet<ExpressionPath<Expression>>(out path);
             
-            // register the path to here in the extractor
-            QueryDataExtractor<TTemplateExpression> extractor = null;
-            state.TryGet<QueryDataExtractor<TTemplateExpression>>(out extractor);
-            ExpressionPath<MethodCallExpression> path = null;
-            state.TryGet<ExpressionPath<MethodCallExpression>>(out path);
+            switch (expression.NodeType) {
+                case ExpressionType.ArrayLength:
+                case ExpressionType.Convert:
+                case ExpressionType.ConvertChecked:
+                case ExpressionType.Negate:
+                case ExpressionType.NegateChecked:
+                case ExpressionType.Not:
+                case ExpressionType.Quote:
+                case ExpressionType.TypeAs:
+                case ExpressionType.UnaryPlus:
+                    state = state.Add<ExpressionPath<UnaryExpression>>(path.Add<UnaryExpression>(e => e.NodeType == expression.NodeType ? e as UnaryExpression : null));
+                    return ParseUnaryExpression((UnaryExpression)expression, state);
+                case ExpressionType.Add:
+                case ExpressionType.AddChecked:
+                case ExpressionType.Divide:
+                case ExpressionType.Modulo:
+                case ExpressionType.Multiply:
+                case ExpressionType.MultiplyChecked:
+                case ExpressionType.Power:
+                case ExpressionType.Subtract:
+                case ExpressionType.SubtractChecked:
+                case ExpressionType.And:
+                case ExpressionType.Or:
+                case ExpressionType.ExclusiveOr:
+                case ExpressionType.LeftShift:
+                case ExpressionType.RightShift:
+                case ExpressionType.AndAlso:
+                case ExpressionType.OrElse:
+                case ExpressionType.Equal:
+                case ExpressionType.NotEqual:
+                case ExpressionType.GreaterThanOrEqual:
+                case ExpressionType.GreaterThan:
+                case ExpressionType.LessThan:
+                case ExpressionType.LessThanOrEqual:
+                case ExpressionType.Coalesce:
+                case ExpressionType.ArrayIndex:
+                    return ParseBinaryExpression ((BinaryExpression)expression, state);
+                case ExpressionType.Conditional:
+                    return ParseConditionalExpression ((ConditionalExpression)expression, state);
+                case ExpressionType.Constant:
+                    return ParseConstantExpression ((ConstantExpression)expression, state);
+                case ExpressionType.Invoke:
+                    return ParseInvocationExpression ((InvocationExpression)expression, state);
+                case ExpressionType.Lambda:
+                    return ParseLambdaExpression ((LambdaExpression)expression, state);
+                case ExpressionType.MemberAccess:
+                    return ParseMemberExpression ((MemberExpression)expression, state);
+                case ExpressionType.Call:
+                    return ParseMethodCallExpression ((MethodCallExpression)expression, state);
+                case ExpressionType.New:
+                    return ParseNewExpression ((NewExpression)expression, state);
+                case ExpressionType.NewArrayBounds:
+                case ExpressionType.NewArrayInit:
+                    return ParseNewArrayExpression ((NewArrayExpression)expression, state);
+                case ExpressionType.MemberInit:
+                    return ParseMemberInitExpression ((MemberInitExpression)expression, state);
+                case ExpressionType.ListInit:
+                    return ParseListInitExpression ((ListInitExpression)expression, state);
+                case ExpressionType.Parameter:
+                    return ParseParameterExpression ((ParameterExpression)expression, state);
+                case ExpressionType.TypeIs:
+                    return ParseTypeBinaryExpression ((TypeBinaryExpression)expression, state);
+            }
             
-            path.Register(name, extractor);
-           
-            return target;
+            return expression;
+        }
+        
+        #endregion tracking path methods
+
+        #region ICompositeVisitor[MethodCallExpression] implementation
+        public Expression Visit (TTemplateExpression target, IVisitState state)
+        {
+            state = state.Add<QueryDataExtractor<TTemplateExpression>>(new QueryDataExtractor<TTemplateExpression>());
+            state = state.Add<ExpressionPath<TTemplateExpression>>(new ExpressionPath<TTemplateExpression>(e => e));
+            return ParseExpression(target, state);
         }
         #endregion
         
+        #region ICompositeVisitor implementation
+        public ICompositeVisitor<TRequiredExpression> GetVisitor<TRequiredExpression> (TRequiredExpression target) where TRequiredExpression : Expression
+        {
+            return this as ICompositeVisitor<TRequiredExpression>;
+        }
+        #endregion
         
-        class ExpressionPath<TExpression>
+        sealed class ExpressionPath<TExpression>
             where TExpression : Expression
         {
             private readonly Expression<Func<TTemplateExpression, TExpression>> Path;
@@ -129,7 +260,8 @@ namespace Epic.Linq.Expressions.Templates
                 dataExtractor.Register<TExpression>(name, Path);
             }
         }
+        
+        
     }
-    
 }
 
