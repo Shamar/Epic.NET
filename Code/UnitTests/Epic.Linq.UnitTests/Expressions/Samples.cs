b2aa4c3dfb23b19cd066c92138fda35ea1279a1e
Giacomo Tesio <giacomo@tesio.it>
2011-11-23 23:36:29 +0100

    removed some line from Samples, since the mono bug has been fixed in master

--- Code/UnitTests/Epic.Linq.UnitTests/Expressions/Samples.cs
+++ Code/UnitTests/Epic.Linq.UnitTests/Expressions/Samples.cs
@@ -188,13 +188,6 @@ namespace Epic.Linq.Expressions
         
         public static MemberInitExpression GetNewMemberInitExpression()
         {
-            /*
-             * The commented lines don't compile with Mono
-             * see https://bugzilla.novell.com/show_bug.cgi?id=730565
-             *
-            Expression<Func<string, ClassToTestMemberBindings>> dummy = s => new ClassToTestMemberBindings() { Name = s, Father = { Name = "Father" }, Children = { new ClassToTestMemberBindings(), new ClassToTestMemberBindings() } };
-            MemberInitExpression expression = (MemberInitExpression)dummy.Body;
-             */
             Type collectionType = typeof(ICollection<ClassToTestMemberBindings>);
             MethodInfo getCollectionAdd = collectionType.GetMethod("Add");
             Type type = typeof(ClassToTestMemberBindings);
