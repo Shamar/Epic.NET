64ec1a302174d1848a4f09cf33d5797ac5935af6
Giacomo Tesio <giacomo@tesio.it>
2012-11-08 01:05:26 +0100

    some more extentions

--- Code/Epic.Prelude/Extended/Delegates/FuncExt.cs
+++ Code/Epic.Prelude/Extended/Delegates/FuncExt.cs
@@ -111,7 +111,7 @@ namespace Epic.Extended.Delegates
             else
             {
                 remainders = new object[values.Length - 1];
-                Array.Copy(values, 1, remainders, 0, values.Length - 1);
+                Array.Copy(values, 1, remainders, 0, remainders.Length);
             }
             return result;
         }
@@ -202,11 +202,109 @@ namespace Epic.Extended.Delegates
             }
             else
             {
-                remainders = new object[values.Length - 1];
-                Array.Copy(values, 1, remainders, 0, values.Length - 1);
+                remainders = new object[values.Length - 2];
+                Array.Copy(values, 2, remainders, 0, remainders.Length);
             }
             return result;
         }
+
+        /// <summary>
+        /// Applies an array of objects to a function.
+        /// </summary>
+        /// <returns>
+        /// The result returned by <paramref name="func"/>.
+        /// </returns>
+        /// <param name='values'>
+        /// Values to apply.
+        /// </param>
+        /// <param name='func'>
+        /// Function to apply.
+        /// </param>
+        /// <typeparam name='T1'>
+        /// The 1st type parameter.
+        /// </typeparam>
+        /// <typeparam name='T2'>
+        /// The 2nd type parameter.
+        /// </typeparam>
+        /// <typeparam name='T3'>
+        /// The 3rd type parameter.
+        /// </typeparam>
+        /// <typeparam name='TResult'>
+        /// The type of the result.
+        /// </typeparam>
+        /// <exception cref="ArgumentNullException"><paramref name="values"/> or <paramref name="func"/>
+        /// is <see langword="null"/>.</exception>
+        /// <exception cref="InvalidCastException">The first element of <paramref name="values"/>
+        /// can not be casted to <typeparamref name="T1"/>, the second element can not be
+        /// casted to <typeparamref name="T2"/> or the third element can not be casted to <typeparamref name="T3"/>.</exception>
+        /// <exception cref="MissingValuesException"><paramref name="values"/> contains too few elements
+        /// to be used as arguments for <paramref name="func"/>.</exception>
+        public static TResult ApplyTo<T1, T2, T3, TResult>(this object[] values, Func<T1, T2, T3, TResult> func)
+        {
+            if (null == values)
+                throw new ArgumentNullException("values");
+            if (null == func)
+                throw new ArgumentNullException("func");
+            if (values.Length < 3)
+                throw new MissingValuesException("values", string.Format("Cannot apply {0} objects to a {1}.", values.Length, func.GetType()));
+            TResult result = func((T1)values[0], (T2)values[1], (T3)values[2]);
+            return result;
+        }
+
+        /// <summary>
+        /// Applies an array of objects to a function.
+        /// </summary>
+        /// <returns>
+        /// The result returned by <paramref name="func"/>.
+        /// </returns>
+        /// <param name='values'>
+        /// Values to apply.
+        /// </param>
+        /// <param name='func'>
+        /// Function to apply.
+        /// </param>
+        /// <param name='remainders'>
+        /// Values ignored by <paramref name="func"/>.
+        /// </param>
+        /// <typeparam name='T1'>
+        /// The 1st type parameter.
+        /// </typeparam>
+        /// <typeparam name='T2'>
+        /// The 2nd type parameter.
+        /// </typeparam>
+        /// <typeparam name='T3'>
+        /// The 3rd type parameter.
+        /// </typeparam>
+        /// <typeparam name='TResult'>
+        /// The type of the result.
+        /// </typeparam>
+        /// <exception cref="ArgumentNullException"><paramref name="values"/> or <paramref name="func"/>
+        /// is <see langword="null"/>.</exception>
+        /// <exception cref="InvalidCastException">The first element of <paramref name="values"/>
+        /// can not be casted to <typeparamref name="T1"/>, the second element can not be
+        /// casted to <typeparamref name="T2"/> or the third element can not be casted to <typeparamref name="T3"/>.</exception>
+        /// <exception cref="MissingValuesException"><paramref name="values"/> contains too few elements
+        /// to be used as arguments for <paramref name="func"/>.</exception>
+        public static TResult ApplyTo<T1, T2, T3, TResult>(this object[] values, Func<T1, T2, T3, TResult> func, out object[] remainders)
+        {
+            if (null == values)
+                throw new ArgumentNullException("values");
+            if (null == func)
+                throw new ArgumentNullException("func");
+            if (values.Length < 3)
+                throw new MissingValuesException("values", string.Format("Cannot apply {0} objects to a {1}.", values.Length, func.GetType()));
+            TResult result = func((T1)values[0], (T2)values[1], (T3)values[2]);
+            if (values.Length == 3)
+            {
+                remainders = _empty;
+            }
+            else
+            {
+                remainders = new object[values.Length - 3];
+                Array.Copy(values, 3, remainders, 0, remainders.Length);
+            }
+            return result;
+        }    
     }
 }
 
